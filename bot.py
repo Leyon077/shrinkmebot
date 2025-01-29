bf10f54f8a170491d53d4167a762380817ea9fb8
Leyon077 <mathuiyyer@gmail.com>
2025-01-29 12:31:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,8 @@ async def link_handler(bot, message):
         return
     for link in links:
         try:
-            short_link2 = await get_shortlink(link)
-            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link2}`",quote=True,disable_web_page_preview=True)
+            short_link = await get_shortlink(link)
+            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link}`",quote=True,disable_web_page_preview=True)
         except Exception as e:
             await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
 
@@ -50,7 +50,7 @@ async def get_shortlink(link):
     url = API_URL
     params = {'api': API_KEY, 'url': link}
     url2 = API_URL2
-    params = {'api': API_KEY2, 'url2': short_link}
+    params = {'api': API_KEY2, 'url2': link}
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
