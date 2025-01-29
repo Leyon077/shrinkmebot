416d9aa612fa1c27ae4713933bd222b8c5beff9d
Leyon077 <mathuiyyer@gmail.com>
2025-01-29 12:59:59 +0530

    Create bot2.py

---
+++ bot2.py
@@ -0,0 +1,59 @@
+# ©AKBOTZ
+
+import re
+import aiohttp
+
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY')
+API_URL = environ.get('API_URL')
+API_KEY2 = environ.get('API_KEY2')
+API_URL2 = environ.get('API_URL2')
+
+akbotz = Client('link shortener bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=100)
+
+print("Developer: @AKBotZ , Join & Share Channel")
+print("Bot is Started Now")
+
+@akbotz.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**Hi {message.chat.first_name}!**\n\n"
+        "I'm Link Shortener bot. Just send me link and get short link, You can also send multiple links seperated by a space or enter.\n\n**Developer:** @Leomsle)
+
+
+@akbotz.on_message(filters.private & filters.text & filters.incoming)
+async def link_handler(bot, message):
+    link_pattern = re.compile('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}', re.DOTALL)
+    links = re.findall(link_pattern, message.text)
+    if len(links2) <1:
+        await message.reply("No links Found in this text",quote=True)
+        return
+    for link2 in links2:
+        try:
+            short_link2 = await get_shortlink(link)
+            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link2}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link2}`",quote=True,disable_web_page_preview=True)
+        except Exception as e:
+            await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
+
+
+async def get_shortlink(link2):
+    url2 = API_URL2
+    params = {'api': API_KEY2, 'url2': link2}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url2, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
+
+akbotz.run()
+                   
