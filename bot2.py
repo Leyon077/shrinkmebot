df6745c140f86af84053bbed2bae7ecf9c921c83
Leyon077 <mathuiyyer@gmail.com>
2025-01-30 23:06:18 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -4,18 +4,8 @@ await message.reply("No links Found in this text",quote=True)
         try:
             short_link2 = await
       get_shortlink(short_link)
-            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link2}`",quote=True,disable_web_page_preview=True)
-        except Exception as e:
-            await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
-
-
+            
 async def get_shortlink(short_link):
     url2 = API_URL2
     params = {'api': API_KEY2, 'url2': short_link}
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
-
-
-akbotz.run()
+    
