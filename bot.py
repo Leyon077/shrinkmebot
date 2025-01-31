f102fefbd24185d3761957d83905afb344e0bf9a
Leyon077 <mathuiyyer@gmail.com>
2025-01-31 05:53:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,20 +38,21 @@ async def link_handler(bot, message):
     if len(links) <1:
         await message.reply("No links Found in this text",quote=True)
         return
-    for link in links:
+    for link, link2 in links:
         try:
-            short_link = await get_shortlink(link)
-            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link2}`",quote=True,disable_web_page_preview=True)
+            short_link, short_link2 = await get_shortlink(link)
+            await message.reply(f"𝐇𝐞𝐫𝐞 𝐢𝐬 𝐘𝐨𝐮𝐫 𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤\n\n𝐎𝐫𝐢𝐠𝐢𝐧𝐚𝐥 𝐋𝐢𝐧𝐤: {link}\n\n𝐒𝐡𝐨𝐫𝐭𝐞𝐧𝐞𝐝 𝐋𝐢𝐧𝐤: `{short_link}`, `{short_link2}`",quote=True,disable_web_page_preview=True)
         except Exception as e:
             await message.reply(f'𝐄𝐫𝐫𝐨𝐫: `{e}`', quote=True)
 
 
-async def get_shortlink(link):
+async def get_shortlink(link, link2):
     url = API_URL
-    params = {'api': API_KEY, 'url': link}
+    url2 = API_URL2
+    params = {'api': API_KEY, 'url': link, 'api2': API_KEY2, 'url2': link2}
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
+        async with session.get(url, url2, params=params, raise_for_status=True) as response:
             data = await response.json()
             return data["shortenedUrl"]
 
